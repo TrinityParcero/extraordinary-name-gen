5238127d5f092828b82bff96aba904fafe3dc641
Trin <razyokatt@gmail.com>
2020-02-29 19:41:23 -0500

    hey it works now yay

--- src/name.js
+++ src/name.js
@@ -1,226 +1,236 @@
-//generator button
-let genButton = document.getElementById("genButton");
+let genButton;
+let genBox;
+let generatedText;
 
-//generator box
-let genBox = document.getElementById("generator");
+let origin = {
+    british: [],
+    mediterranean: [],
+    eastEuropean: [],
+    european: [],
+    asian: [],
+    misc: []
+};
 
-const ethBritish = [
-    "Welsh",
-    "Cornish",
-    "English",
-    "Medieval",
-    "Gaelic"
-];
+let britishAllButton;
+let mediterraneanAllButton;
+let eastEuropeanAllButton;
+let europeanAllButton;
+let asianAllButton;
+let miscAllButton;
 
-const ethMediterranean = [
-    "Italian",
-    "Greek",
-    "Ancient Greek",
-    "Arabic"
-];
+let selectedOrigins = [];
 
-const ethEastEuropean = [
-    "Russian",
-    "Polish",
-    "Hungarian",
-    "Finnish",
-    "Armenian"
-];
+const gender = {};
+let selectedGender;
 
-const ethEuropean = [
-    "Spanish",
-    "Dutch",
-    "French",
-    "Gaullic",
-    "German",
-    "Norse"
-];
+let lastNameEnabled;
 
-const ethAsian = [
-    "Chinese",
-    "Japanese",
-    "Indian",
-    "Mongolian",
-    "Korean"
-];
+let numNamesButtons;
+let selectedNumNames;
 
-const ethMisc = [
-    "African",
-    "Amerindian",
-    "Aboriginal",
-    "Hebrew",
-    "Romani"
-];
+let firstNames = [];
+let lastNames = [];
 
-//ethnicity and gender values
-let eths = document.querySelectorAll("input[name=eth]");
-let ethsSelected = [];
-let female = document.getElementById("gender1");
-let male = document.getElementById("gender2");
-let both = document.getElementById("gender3");
+/**
+ * SET UP REFERENCES
+ * gets references to html, adds onclicks
+ */
+const setUpReferences = () => {
+    //generator
+    genButton = document.querySelector('#genButton');
+    genBox = document.getElementById('generator');
+    generatedText = document.querySelector('#generated');
+    genButton.onclick = () => {
+        //clear old values
+        console.log('you clicked the generator button');
+        generatedText.innerHTML = '';
+        firstNames = [];
+        lastNames = [];
+        selectedOrigins = [];
+        selectedNumNames = 0;
 
-//last names and number of names
-let last = document.getElementById("last1");
-let nums = document.querySelectorAll("input[name=num]")
-let numSelected;
+        setUpSearch();
+        showNames();
+    };
 
-//p tag where generated names go
-let p = document.querySelector("#generated");
-
-//select category buttons
-let british = document.querySelector("#britishAll");
-let mediterranean = document.querySelector("#mediterraneanAll");
-let eastEuropean = document.querySelector("#eastEuropeanAll");
-let european = document.querySelector("#europeanAll");
-let asian = document.querySelector("#asianAll");
-let other = document.querySelector("#otherAll");
-
-
-british.addEventListener('click', function () {
-    for (let i = 0; i < 5; i++) {
-        //0-4
-        eths[i].checked = !eths[i].checked;
-    }
-    console.log('you clicked a thing');
-});
-
-mediterranean.addEventListener('click', function () {
-    for (let i = 5; i < 9; i++) {
-        //5-9
-        eths[i].checked = !eths[i].checked;
-    }
-});
-
-eastEuropean.addEventListener('click', function () {
-    for (let i = 9; i < 14; i++) {
-        //10-13
-        eths[i].checked = !eths[i].checked;
-    }
-});
-
-european.addEventListener('click', function () {
-    for (let i = 14; i < 20; i++) {
-        //14-19
-        eths[i].checked = !eths[i].checked;
-    }
-});
-
-asian.addEventListener('click', function () {
-    for (let i = 20; i < 25; i++) {
-        //21-24
-        eths[i].checked = !eths[i].checked;
-    }
-});
-
-other.addEventListener('click', function () {
-    for (let i = 25; i < 31; i++) {
-        //25-30
-        eths[i].checked = !eths[i].checked;
-    }
-});
-
-//SET UP SEARCH. takes input and figures out what the user actually wants
-let setUpSearch = function () {
-
-    //go thru all checkboxes, add checked ones to array
-    for (let i = 0; i < eths.length; i++) {
-        if (eths[i].checked) {
-            ethsSelected[i] = eths[i].value;
+    //ethnicity selection
+    const allOrigins = document.querySelectorAll('input[name=eth]');
+    for (let i = 0; i < allOrigins.length; i++) {
+        if (i < 5) {
+            origin.british.push(allOrigins[i]);
+        } else if (i < 9) {
+            origin.mediterranean.push(allOrigins[i]);
+        } else if (i < 14) {
+            origin.eastEuropean.push(allOrigins[i]);
+        } else if (i < 20) {
+            origin.european.push(allOrigins[i]);
+        } else if (i < 25) {
+            origin.asian.push(allOrigins[i]);
+        } else {
+            origin.misc.push(allOrigins[i]);
         }
     }
 
-    //go thru nums radio, set checked one as numSelected
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i].checked) {
-            numSelected = nums[i].value;
-            //futz with size of genBox
-            if (numSelected == 1) {
-                genBox.style.height = "100px";
-            }
-            if (numSelected == 5) {
-                genBox.style.height = "200px";
-            }
-            if (numSelected == 10) {
-                genBox.style.height = "350px";
+    //select category buttons
+    britishAllButton = document.querySelector('#britishAll');
+    britishAllButton.onclick = () => {
+        console.log('you clicked the british button');
+        for (const button of origin.british) {
+            button.checked = !button.checked;
+        }
+    };
+    mediterraneanAllButton = document.querySelector('#mediterraneanAll');
+    mediterraneanAllButton.onclick = () => {
+        console.log('you clicked the mediterrean button');
+        for (const button of origin.mediterranean) {
+            button.checked = !button.checked;
+        }
+    };
+    eastEuropeanAllButton = document.querySelector('#eastEuropeanAll');
+    eastEuropeanAllButton.onclick = () => {
+        console.log('you clicked the east european button');
+        for (const button of origin.eastEuropean) {
+            button.checked = !button.checked;
+        }
+    };
+    europeanAllButton = document.querySelector('#europeanAll');
+    europeanAllButton.onclick = () => {
+        console.log('you clicked the european button');
+        for (const button of origin.european) {
+            button.checked = !button.checked;
+        }
+    };
+    asianAllButton = document.querySelector('#asianAll');
+    asianAllButton.onclick = () => {
+        console.log('you clicked the asian button');
+        for (const button of origin.asian) {
+            button.checked = !button.checked;
+        }
+    };
+    miscAllButton = document.querySelector('#otherAll');
+    miscAllButton.onclick = () => {
+        console.log('you clicked the miscellaneous button');
+        for (const button of origin.misc) {
+            button.checked = !button.checked;
+        }
+    };
+
+    //gender selection
+    gender.female = document.getElementById('gender1');
+    gender.male = document.getElementById('gender2');
+    gender.both = document.getElementById('gender3');
+
+    lastNameEnabled = document.getElementById('last1');
+    numNamesButtons = document.querySelectorAll('input[name=num]');
+
+    console.log('finished setup!');
+};
+
+window.onload = setUpReferences();
+
+// SET UP SEARCH. takes input and figures out what the user actually wants
+const setUpSearch = () => {
+    // check for selected origins
+    for (let category of Object.values(origin)) {
+        for (let option of Object.values(category)) {
+            if (option.checked) {
+                selectedOrigins.push(option.value);
             }
         }
     }
-    //for each ethselected
-    for (let i = 0; i < ethsSelected.length; i++) {
 
-        //string property name to search names for
+    // check for selected gender
+    for (let option of Object.values(gender)) {
+        if (option.checked) {
+            selectedGender = option.value;
+        }
+    }
+
+    // check for selected numNames
+    for (let option of numNamesButtons) {
+        if (option.checked) {
+            selectedNumNames = option.value;
+        }
+    }
+
+    console.log(`origins: ${selectedOrigins}`);
+    console.log(`gender: ${selectedGender}`);
+    console.log(`number: ${selectedNumNames}`);
+
+    // futz with size of genBox
+    if (selectedNumNames == 1) {
+        genBox.style.height = '100px';
+    } else if (selectedNumNames == 5) {
+        genBox.style.height = '200px';
+    } else if (selectedNumNames == 10) {
+        genBox.style.height = '350px';
+    }
+
+    // for each ethselected
+    for (let i = 0; i < selectedOrigins.length; i++) {
+        // string property name to search names for
         let propName;
 
-        //check the gender buttons
-        if (female.checked) {
+        // check the gender buttons
+        if (selectedGender === 'female') {
             //return eth + Female to firstNames
-            if (ethsSelected[i] != undefined) {
+            if (selectedOrigins[i] != undefined) {
                 //welsh is a special case
                 //names are unisex
-                if (ethsSelected[i] == "welsh") {
-                    firstNames += names["welsh"];
-                }
-                else {
-                    propName = ethsSelected[i] + "Female";
-                    firstNames += names[propName];
-                }
-
-            }
-        }
-        if (male.checked) {
-            //return eth + Male to firstNames
-            if (ethsSelected[i] != undefined) {
-                //welsh is a special case
-                //names are unisex
-                if (ethsSelected[i] == "welsh") {
-                    firstNames += names["welsh"];
-                }
-                else {
-                    propName = ethsSelected[i] + "Male";
-                    firstNames += names[propName];
+                if (selectedOrigins[i] == 'welsh') {
+                    firstNames.push(...names['welsh']);
+                } else {
+                    propName = `${selectedOrigins[i]}Female`;
+                    firstNames.push(...names[propName]);
                 }
             }
-        }
-        if (both.checked) {
+        } else if (selectedGender === 'male') {
+            // return eth + Male to firstNames
+            if (selectedOrigins[i] != undefined) {
+                // welsh is a special case
+                // names are unisex
+                if (selectedOrigins[i] == 'welsh') {
+                    firstNames.push(names['welsh']);
+                } else {
+                    propName = `${selectedOrigins[i]}Male`;
+                    firstNames.push(...names[propName]);
+                }
+            }
+        } else {
             //return eth + Female and eth + Male to firstNames
-            if (ethsSelected[i] != undefined && ethsSelected != "welsh") {
-                propName = ethsSelected[i] + "Female";
-                firstNames += names[propName];
+            if (selectedOrigins[i] != undefined && selectedOrigins != 'welsh') {
+                propName = `${selectedOrigins[i]}Female`;
+                firstNames.push(...names[propName]);
             }
-            if (ethsSelected[i] != undefined && ethsSelected != "welsh") {
-                propName = ethsSelected[i] + "Male";
-                firstNames += names[propName];
-            }
-            else {
+            if (selectedOrigins[i] != undefined && selectedOrigins != 'welsh') {
+                propName = `${selectedOrigins[i]}Male`;
+                firstNames.push(...names[propName]);
+            } else {
                 //welsh is a special case
                 //names are unisex
-                if (ethsSelected[i] == "welsh") {
-                    firstNames += names["welsh"];
+                if (selectedOrigins[i] == 'welsh') {
+                    firstNames.push(...names['welsh']);
                 }
             }
         }
 
         //check the last name button
-        if (last.checked) {
-            //return eth + Family to lastNames 
-            if (ethsSelected[i] != undefined) {
-                propName = ethsSelected[i] + "Family";
+        if (lastNameEnabled.checked) {
+            //return eth + Family to lastNames
+            if (selectedOrigins[i] != undefined) {
+                propName = `${selectedOrigins[i]}Family`;
                 //not all eths have a family category
                 if (names[propName] != undefined) {
-                    lastNames += names[propName];
+                    lastNames.push(...names[propName]);
                 }
-
             }
         }
-
     }
+};
 
-}
-
-let showNames = function () {
-
-    //for numselected, generate names
-    for (let i = 0; i < numSelected; i++) {
+const showNames = () => {
+    //for numNames, generate names
+    for (let i = 0; i < selectedNumNames; i++) {
         let firstName;
         let lastName;
         //generate first name
@@ -228,34 +238,16 @@ let showNames = function () {
         firstName = firstNames[rando];
 
         //if last is true, generate last name
-        if (last.checked) {
+        if (lastNameEnabled) {
             rando = Math.floor(Math.random() * Math.floor(lastNames.length));
             lastName = lastNames[rando];
         }
 
-        //put generated names in generated 
-
+        //put generated names in generated
         if (lastName != undefined) {
-            p.innerHTML += (firstName + " " + lastName + "<br>");
-        }
-        else {
-            p.innerHTML += (firstName + "<br>");
+            generatedText.innerHTML += firstName + ' ' + lastName + '<br>';
+        } else {
+            generatedText.innerHTML += firstName + '<br>';
         }
     }
-
-}
-
-//onclick of generator button
-//call name functions
-genButton.addEventListener('click', function () {
-
-    //clear old values
-    p.innerHTML = "";
-    firstNames = [];
-    lastNames = [];
-    ethsSelected = [];
-    numSelected = 0;
-
-    setUpSearch();
-    showNames();
-});
+};
